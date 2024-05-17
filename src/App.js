e555469417273ab98db217fd3710af2c32c80574
rameti sivamani <sivamanirameti494@gmail.com>
2024-05-17 20:03:30 +0530

    new changes

--- src/App.js
+++ src/App.js
@@ -4,40 +4,76 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 //Code to import Budget.js
 import Budget from './components/Budget';
-
+import Remaining from './components/Remaining';
+import ExpenseTotal from './components/ExpenseTotal';
 // Add code to import the other components here under
 
 
 import { AppProvider } from './context/AppContext';
+import ExpenseList from './components/ExpenseList';
+import ExpenseItem from './components/ExpenseItem';
+import AllocationForm from './components/AllocationForm';
 const App = () => {
     return (
         <AppProvider>
             <div className='container'>
                 <h1 className='mt-3'>Company's Budget Allocation</h1>
-                    <div className='row mt-3'>
-                        {
-                            /* Add Budget component here */
-                        }        
+                <div className='row mt-3'>
+                    {
+                        /* Add Budget component here */
+                        <div className='col-sm'>
+                            <Budget />
+                        </div>
+                    }
 
-                        {
-                            /* Add Remaining component here*/
-                        }        
+                    {
+                        /* Add Remaining component here*/
+                        <div className='col-sm'>
+                            <Remaining />
+                        </div>
+                    }
 
-                        {
-                            /* Add ExpenseTotal component here */
-                        }        
-                       
-                        {
-                            /* Add ExpenseList component here */
-                        }         
+                    {
+                        /* Add ExpenseTotal component here */
+                        <div className='col-sm'>
+                            <ExpenseTotal />
+                        </div>
+                    }
 
-                        {
-                            /* Add ExpenseItem component here */
-                        }        
+                    {
+                        /* Add ExpenseList component here */
+                        <div className='row'>
 
-                        {
-                            /* Add AllocationForm component here under */
-                        }        
+                            <div className='col'>
+                                <ExpenseList />
+                            </div>
+
+                        </div>
+                    }
+
+                    {
+                    
+                    /* Add ExpenseItem component here */
+                    <div className='row'>
+
+                            <div className='col'>
+                                <ExpenseItem />
+                            </div>
+
+                        </div>
+                    }
+
+                    {
+                        /* Add AllocationForm component here under */
+                        <div className='row'>
+
+                            <div className='col'>
+                                <br></br>
+                                <AllocationForm />
+                            </div>
+
+                        </div>
+                    }
 
                 </div>
             </div>
--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,1 +1,28 @@
+import React, { useContext, useState } from 'react';
+import { AppContext } from '../context/AppContext';
+const Budget = () => {
+    const { budget,expenses } = useContext(AppContext);
+    const [newBudget, setNewBudget] = useState(budget);
+    const totalExpenses = expenses.reduce((total, item) => {
+        return (total += item.cost);
+    }, 0);
+    const handleBudgetChange = (event) => {
+        if(event.target.value > 20000){
+            alert(`The budget should not exceed 20000`)
+        }
+        else if(event.target.value < totalExpenses){
+            alert(`You cannot reduce the budget value lower than the spending`)
 
+        }
+        else{
+            setNewBudget(event.target.value);
+        }
+    }
+    return (
+<div className='alert alert-secondary'>
+<span>Budget: £ </span>
+<input type="number" step="10" min={1} max={20000} value={newBudget} onChange={handleBudgetChange}></input>
+</div>
+    );
+};
+export default Budget;
